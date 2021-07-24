9d1452868e30b90e6310a800e7511c7e1fde00e2
Nguyen Minh Quang <gdra136@gmail.com>
2021-07-24 23:27:04 +0700

    - fix multer (save image in another format) - show product.images[0] in my post list

--- src/utils/multer.ts
+++ src/utils/multer.ts
@@ -1,6 +1,17 @@
 import multer from "multer";
 import env from "@/config/env";
+import path from "path";
 
-const multerUpload = multer({ dest: env("UPLOAD_DIR") });
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, env("UPLOAD_DIR")+'/');
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+const multerUpload = multer({
+    storage: storage
+});
 
 export default multerUpload;
